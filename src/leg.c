b44b3bd8c98c3022e2f32e35bce71ca4bd6f3996
fomgleb <fomglib@gmail.com>
2024-10-23 10:19:23 +0300

    Add input_leg_t structure

--- src/leg.c
+++ src/leg.c
@@ -12,6 +12,20 @@ leg_create_output_leg(volatile uint8_t* ddr, volatile uint8_t* port, volatile ui
     return new_output_leg;
 }
 
+input_leg_t
+leg_create_input_leg(volatile uint8_t* ddr, volatile uint8_t* port, const volatile uint8_t* pin,
+                     volatile uint8_t leg_index) {
+    *ddr &= ~(1 << leg_index);
+    *port |= (1 << leg_index);
+    input_leg_t new_input_leg = {
+        .ddr = ddr,
+        .port = port,
+        .pin = pin,
+        .leg_index = leg_index,
+    };
+    return new_input_leg;
+}
+
 void
 leg_set(output_leg_t* leg) {
     *leg->port |= (1 << leg->leg_index);
@@ -39,4 +53,9 @@ leg_drop_set_drop(output_leg_t* leg) {
     leg_drop(leg);
     leg_set(leg);
     leg_drop(leg);
+}
+
+bool
+leg_get(const input_leg_t* leg) {
+    return *leg->pin & (1 << leg->leg_index);
 }
--- src/leg.h
+++ src/leg.h
@@ -2,6 +2,7 @@
 #define ADC_LEG_H_
 
 #include <inttypes.h>
+#include "boolean.h"
 
 typedef struct {
     const volatile uint8_t* ddr;
@@ -9,11 +10,21 @@ typedef struct {
     volatile uint8_t leg_index;
 } output_leg_t;
 
+typedef struct {
+    const volatile uint8_t* ddr;
+    const volatile uint8_t* port;
+    const volatile uint8_t* pin;
+    const volatile uint8_t leg_index;
+} input_leg_t;
+
 output_leg_t leg_create_output_leg(volatile uint8_t* ddr, volatile uint8_t* port, volatile uint8_t leg_index);
+input_leg_t leg_create_input_leg(volatile uint8_t* ddr, volatile uint8_t* port, const volatile uint8_t* pin,
+                                 volatile uint8_t leg_index);
 void leg_set(output_leg_t* leg);
 void leg_drop(output_leg_t* leg);
 void leg_drop_set(output_leg_t* leg);
 void leg_set_drop(output_leg_t* leg);
 void leg_drop_set_drop(output_leg_t* leg);
+bool leg_get(const input_leg_t* leg);
 
 #endif // ADC_LEG_H_
